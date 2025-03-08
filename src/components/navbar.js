3e3f2fdcff8aee16c042b691e8f92ba6d5637950
Mohammed Minhaj <nijulinux@gmail.com>
2025-03-08 12:11:30 +0530

    fix(navbar): fixed the anchors

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -14,18 +14,19 @@ export const navbar = `
           aria-current="page"
           class="inline-block rounded-lg px-2 py-1 text-sm font-medium text-gray-900 transition-all duration-200 hover:bg-gray-100 hover:text-gray-900"
           href="#"
-          >WLL FILL !!</a
+          >Home</a
         >
         <a
           class="inline-block rounded-lg px-2 py-1 text-sm font-medium text-gray-900 transition-all duration-200 hover:bg-gray-100 hover:text-gray-900"
-          href="#"
-          >WLL FILL !!</a
+          href="https://en.wikipedia.org/wiki/International_Women%27s_Day"
+          >About</a
         >
       </div>
       <div class="flex items-center justify-end gap-3">
         <a
+        href="https://github.com/Minhajxdd/womens_day_challenge"
           class="cursor-pointer inline-flex items-center justify-center rounded-xl bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm transition-all duration-150 hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
-          >About</a
+          >Git Hub</a
         >
       </div>
     </div>
