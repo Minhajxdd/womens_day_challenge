c406440b129b0e0ceb1ab3baa8dc01e279d01d3b
Mohammed Minhaj <nijulinux@gmail.com>
2025-03-08 11:29:36 +0530

    feat(navbar): added navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,11 +1,12 @@
+export const navbar = `
 <header
-  class="fixed inset-x-0 top-0 z-30 mx-auto w-full max-w-screen-md border border-gray-100 bg-white/80 py-3 shadow backdrop-blur-lg md:top-6 md:rounded-3xl lg:max-w-screen-lg"
+  class="fixed inset-x-0 top-0 z-30 mx-auto w-full max-w-screen-md border border-gray-100 bg-white  py-3 shadow backdrop-blur-lg md:top-6 md:rounded-3xl lg:max-w-screen-lg"
 >
   <div class="px-4">
     <div class="flex items-center justify-between">
       <div class="flex shrink-0">
         <a aria-current="page" class="flex items-center" href="/">
-          <i class="bx bxs-book"></i>
+          <img class="w-[55px]" src="https://cdn.pixabay.com/photo/2018/03/04/13/19/womens-day-3198006_1280.png" alt="Image Description">
         </a>
       </div>
       <div class="hidden md:flex md:items-center md:justify-center md:gap-5">
@@ -13,20 +14,21 @@
           aria-current="page"
           class="inline-block rounded-lg px-2 py-1 text-sm font-medium text-gray-900 transition-all duration-200 hover:bg-gray-100 hover:text-gray-900"
           href="#"
-          >How it works</a
+          >WLL FILL !!</a
         >
         <a
           class="inline-block rounded-lg px-2 py-1 text-sm font-medium text-gray-900 transition-all duration-200 hover:bg-gray-100 hover:text-gray-900"
           href="#"
-          >Pricing</a
+          >WLL FILL !!</a
         >
       </div>
       <div class="flex items-center justify-end gap-3">
         <a
           class="cursor-pointer inline-flex items-center justify-center rounded-xl bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm transition-all duration-150 hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
-          >Logout</a
+          >About</a
         >
       </div>
     </div>
   </div>
 </header>
+`
